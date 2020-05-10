9106f1bf3ec9b2c76747226b22532721a50daa77
Han Yan <hanprogramming@gmail.com>
2020-05-10 11:52:40 -0600

    Example of how to use args and rest operator in C#

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
-namespace FirstProject
+namespace dotnetVSCode
 {
     class Program
     {
+        public static double Sum(params double[] args) // This is how I set Rest Operator for variable number of params in C#
+        {
+            // Array.Aggregate is C#'s array reduce method
+            return args.Aggregate((accum, x) => accum + x); 
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double sum = Program.Sum(1, 2, 3, 4); // See?
+            Console.WriteLine($"The sum is {sum}");
         }
     }
 }
