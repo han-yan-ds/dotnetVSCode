106b00c04fe0a1dfbfc931ff0473a0ab38a42825
Han Yan <hanprogramming@gmail.com>
2020-05-10 12:05:29 -0600

    Explaining the out parameter modifier

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -4,9 +4,14 @@ namespace FirstProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int parseResult; // nothing now, 78 later
+            bool isParsed = int.TryParse("78", out parseResult); // true (return value of TryParse because it can parse "78" into an int
+
+            Console.WriteLine("This is how an out modifier works in parameters... \nThe out parameter assigns a value to the variable outside the function's scope");
+            Console.WriteLine("\tparseResult: {0} - this is the out parameter", parseResult);
+            Console.WriteLine("\tisParsed: {0} - this is actually the return value of TryParse", isParsed);
         }
     }
 }
