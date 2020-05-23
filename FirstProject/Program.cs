69c60c55ed02e5a62d02e742f5b9372779588de5
Han Yan <hanprogramming@gmail.com>
2020-05-23 12:34:08 -0600

    Cleaned up delegate notes by using Action

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -2,36 +2,24 @@
 
 namespace Delegates
 /*
-* A System.Delegate is an object created using the delegate keyword (see PhotoProcessor.cs)
-* This object has:
-* 1) Target (what the delegate is running its functions on)
-* 2) Method (typically something like () => function(target))
+* This is an example of how to use the Action built-in delegate in .NET
 * 
-* IMPORTANT: (A delegate is essentially a way to load methods)
-*
-* A delegate is converted to a System.MulticastDelegate when it gets a function added onto it (see "+=" below)
-* A MulticastDelegate object has:
-* 1) _invocationCount (# of functions assigned to it to run)
-* 2) _invocationList (list of items similar to Method in System.Delegate)
+* Func is also a built-in delegate, but Func returns a value; Action doesn't.
 */
 {
     class Program
     {
         static void Main(string[] args)
         {
-            PhotoProcessor processor = new PhotoProcessor();
             PhotoFilters filters = new PhotoFilters();
             object photo = new object();
 
-            // PhotoFilterHandler is a delegate in the PhotoProcessor class, assigned to a function
-            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
-            // delegates (such as filterHandler) can use the += operator to add another function to be run
-            filterHandler += filters.ApplyContrast;
-            // delegates can also use custom functions and lambda expressions
-            filterHandler += (ph => Console.WriteLine("Removed Redeye"));
+            Action<object> processPhoto = filters.ApplyBrightness;
+            processPhoto += filters.ApplyContrast;
+            processPhoto += (ph => Console.WriteLine("Removed Redeye"));
 
             // Now, run 3 functions (loaded onto Delegate) on photo
-            processor.Process(photo, filterHandler);
+            processPhoto(photo);
         }
     }
 }
